64840b4eb5b118613e4c5429d79c205c5240c551
ArvinRa1 <80069023+ArvinRa1@users.noreply.github.com>
2021-03-08 18:15:53 +0000

    Added "SomeClasses" file to the repository.
    This file contains no answers for the tasks.

---
+++ EMPythonTest/SomeClasses.py
@@ -0,0 +1,44 @@
+# Task 8
+class Rectangle:
+    pass
+
+
+class Circle:
+    pass
+
+
+# Task 9
+class Product:
+    pass
+
+
+# Task 10
+class PhoneBook:
+    def add_number(self, name, number):
+        """
+       Add a contact or throw InvalidOperationException if it is already present
+        :param number: an integer number
+        :param name: a string
+       """
+        pass
+
+    def del_number(self, name):
+        """
+        Delete a contact or throw InvalidOperationException if it is not present
+        :param name: a string
+        """
+        pass
+
+    def size(self):
+        """
+        :return: current size of PhoneBook
+        """
+        return None
+
+    def get_all(self):
+        """
+        return: a string representation of all entries in the format 'name: number',
+        merged and separated by ';' and a space. The entries should be sorted alphabetically
+        """
+
+        return ""
