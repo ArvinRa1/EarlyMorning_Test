0bc07b19d83f6af2972376996792ebc649b7d238
ArvinRa1 <80069023+ArvinRa1@users.noreply.github.com>
2021-03-08 18:08:19 +0000

    Added basic Function file to the Repository.
    This file contains no answers in the tasks.

---
+++ EMPythonTest/BasicFunctions.py
@@ -0,0 +1,46 @@
+# Task 1
+def average(a, b):
+    a + b / 2
+
+
+# Task 2
+def reverse_list(input_list):
+    return None
+
+
+# Task 3
+def sort_numbers_descending(number_list):
+    return None
+
+
+# Task 4
+def add_indices(string_list):
+    return None
+
+
+# Task 5
+def capitalize_last_letter_in_each_word(string):
+    return None
+
+
+# Task 6
+def element_wise_merge(list1, list2):
+    """
+    Function that performs element-wise merge of list elements, inserting blank space in between
+    :param list1: list of string
+    :param list2: list of strings of the same length as list1
+    :return: new list of merged strings
+    """
+    return None
+
+
+# Task 7
+def execute_safely(operator, a, b):
+    """
+    Function that executes operator on arguments (a, b) -- or returns -1
+    :param operator: some real-valued function, taking on input two real arguments
+    :param a: a real number
+    :param b: a real number
+    :return: operator evaluated on (a,b) -- or -1 if this operation would be illegal
+    """
+    return operator(a, b)
